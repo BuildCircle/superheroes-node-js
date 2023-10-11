de13e5a928150a5e7bb20961cdbb7368ab0eb461
Chris <chris.sewell@buildcircle.co.uk>
2023-10-11 18:13:48 +0100

    remove type checking for js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ const app = express();
 
 app.get('/battle', (req, res) => {
 
-  if(typeof req.query.hero === 'string' && typeof req.query.villain === 'string'){
-    const result = battle(req.query.hero, req.query.villain)
-    res.send(result);
-  }
-  res.send(400);
+  const result = battle(req.query.hero, req.query.villain)
+  res.send(result);
+  
 });
 
 module.exports = app;
