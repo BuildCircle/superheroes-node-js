6cef829c1e8bba3e2015d8252ea85763e6a615de
amarriott <alex.marriott@buildcircle.co.uk>
2025-10-08 12:59:28 +0100

    ta-da

--- src/battle.js
+++ src/battle.js
@@ -1,12 +1,23 @@
 const getCharacters = require('./getCharacters')
 
-function battle(heroName, villainName) {
+function battle(hero, villain) {
+  const heroCharacter = findCharacter(hero.name, hero.type)
+  const villainCharacter = findCharacter(villain.name, villain.type)
+
+  return heroCharacter.score >= villainCharacter.score ? heroCharacter : villainCharacter
+}
+
+function findCharacter(name, type) {
   const characters = getCharacters()
 
-  const hero = characters.items.find(e => e.name === heroName)
-  const villain = characters.items.find(e => e.name === villainName)
-
-  return hero.score >= villain.score ? hero : villain
+  let character
+  for (let i = 0; i < characters.items.length; i++) {
+    if (characters.items[i].name === name && characters.items[i].type === type) {
+      character = characters.items[i]
+    }
+  }
+  return character
 }
+
 
 module.exports = battle
